716776713212c33966fbe42a4ad756869603f396
Luca Favini <luca.favini.02@gmail.com>
2023-03-09 20:05:02 +0100

    greedy

--- src/Greedy.java
+++ src/Greedy.java
@@ -1,4 +1,22 @@
 public class Greedy {
   
+
+  public Vertice getStart(Grafo g) {
+
+    Vertice max = null;
+
+    for (int y = 0; y < g.valori[0].length; y++) {
+      for (int x = 0; x < g.valori.length; x++) {
+        var cur = g.valori[x][y]; 
+        if (!cur.lock) {
+          if (max == null || max.valore < cur.valore) {
+            max = cur;
+          }
+        } 
+      }
+    }
+
+    return max;
+  } 
   
 }

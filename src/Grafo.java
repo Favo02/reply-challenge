59b13407a271b7135433432873c3494414c3049d
Luca Favini <luca.favini.02@gmail.com>
2023-03-09 19:12:20 +0100

    grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grafo {
   Vertice[][] valori;
 
@@ -22,102 +25,107 @@ public class Grafo {
       }
     }
 
+    List<Vertice> wormholes = new ArrayList<>(); 
+
     for (int y = 0; y < input.r; y++) {
       for (int x = 0; x < input.c; x++) {
 
-        // no wormhole
-        if (!valori[x][y].wormhole) {
-
-          // angolo alto sinistra
-          if (y == 0 && x == 0) {
-            valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-            valori[x][y].addAdiacente(valori[x][y-1]); // down
-            valori[x][y].addAdiacente(valori[x+1][y]); // right
-            valori[x][y].addAdiacente(valori[input.c-1][y]); // left
-            
-            continue;
-          }
-
-          // angolo alto destra
-          if (y == 0 && x == input.c-1) {
-            valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-            valori[x][y].addAdiacente(valori[x][y-1]); // down
-            valori[x][y].addAdiacente(valori[0][y]); // right
-            valori[x][y].addAdiacente(valori[x-1][y]); // left
-            
-            continue;
-          }
-          
-          // angolo basso sinistra
-          if (y == input.r-1 && x == 0) {
-            valori[x][y].addAdiacente(valori[x][y+1]); // up
-            valori[x][y].addAdiacente(valori[x][0]); // down
-            valori[x][y].addAdiacente(valori[x+1][y]); // right
-            valori[x][y].addAdiacente(valori[input.c-1][y]); // left
-            
-            continue;
-          }
-          
-          // angolo basso destra
-          if (y == input.r-1 && x == input.c-1) {
-            valori[x][y].addAdiacente(valori[x][y+1]); // up
-            valori[x][y].addAdiacente(valori[x][0]); // down
-            valori[x][y].addAdiacente(valori[0][y]); // right
-            valori[x][y].addAdiacente(valori[x-1][y]); // left
-            
-            continue;
-          }
-
-
-
-          // prima riga
-          if (y == 0) {
-            valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-            valori[x][y].addAdiacente(valori[x][y-1]); // down
-            valori[x][y].addAdiacente(valori[x+1][y]); // right
-            valori[x][y].addAdiacente(valori[x-1][y]); // left
-            
-            continue;
-          }
-
-          // ultima riga
-          if (y == input.r) {
-            valori[x][y].addAdiacente(valori[x][y+1]); // up
-            valori[x][y].addAdiacente(valori[x][0]); // down
-            valori[x][y].addAdiacente(valori[x+1][y]); // right
-            valori[x][y].addAdiacente(valori[x-1][y]); // left
-
-            continue;
-          }
-
-          // prima colonna
-          if (x == 0) {
-            valori[x][y].addAdiacente(valori[x][y+1]); // up
-            valori[x][y].addAdiacente(valori[x][y-1]); // down
-            valori[x][y].addAdiacente(valori[x+1][y]); // right
-            valori[x][y].addAdiacente(valori[input.c-1][y]); // left
-
-            continue;
-          }
-
-          // ultima colonna
-          if (x == input.c) {
-            valori[x][y].addAdiacente(valori[x][y+1]); // up
-            valori[x][y].addAdiacente(valori[x][y-1]); // down
-            valori[x][y].addAdiacente(valori[0][y]); // right
-            valori[x][y].addAdiacente(valori[x-1][y]); // left
-
-            continue;
-          }
-
-          valori[x][y].addAdiacente(valori[x][y+1]); // up
+        // angolo alto sinistra
+        if (y == 0 && x == 0) {
+          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
           valori[x][y].addAdiacente(valori[x][y-1]); // down
           valori[x][y].addAdiacente(valori[x+1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
+          
+          continue;
+        }
+
+        // angolo alto destra
+        if (y == 0 && x == input.c-1) {
+          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
+          valori[x][y].addAdiacente(valori[x][y-1]); // down
+          valori[x][y].addAdiacente(valori[0][y]); // right
           valori[x][y].addAdiacente(valori[x-1][y]); // left
+          
+          continue;
+        }
+        
+        // angolo basso sinistra
+        if (y == input.r-1 && x == 0) {
+          valori[x][y].addAdiacente(valori[x][y+1]); // up
+          valori[x][y].addAdiacente(valori[x][0]); // down
+          valori[x][y].addAdiacente(valori[x+1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
+          
+          continue;
+        }
+        
+        // angolo basso destra
+        if (y == input.r-1 && x == input.c-1) {
+          valori[x][y].addAdiacente(valori[x][y+1]); // up
+          valori[x][y].addAdiacente(valori[x][0]); // down
+          valori[x][y].addAdiacente(valori[0][y]); // right
+          valori[x][y].addAdiacente(valori[x-1][y]); // left
+          
+          continue;
         }
 
 
+
+        // prima riga
+        if (y == 0) {
+          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
+          valori[x][y].addAdiacente(valori[x][y-1]); // down
+          valori[x][y].addAdiacente(valori[x+1][y]); // right
+          valori[x][y].addAdiacente(valori[x-1][y]); // left
+          
+          continue;
+        }
+
+        // ultima riga
+        if (y == input.r) {
+          valori[x][y].addAdiacente(valori[x][y+1]); // up
+          valori[x][y].addAdiacente(valori[x][0]); // down
+          valori[x][y].addAdiacente(valori[x+1][y]); // right
+          valori[x][y].addAdiacente(valori[x-1][y]); // left
+
+          continue;
+        }
+
+        // prima colonna
+        if (x == 0) {
+          valori[x][y].addAdiacente(valori[x][y+1]); // up
+          valori[x][y].addAdiacente(valori[x][y-1]); // down
+          valori[x][y].addAdiacente(valori[x+1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
+
+          continue;
+        }
+
+        // ultima colonna
+        if (x == input.c) {
+          valori[x][y].addAdiacente(valori[x][y+1]); // up
+          valori[x][y].addAdiacente(valori[x][y-1]); // down
+          valori[x][y].addAdiacente(valori[0][y]); // right
+          valori[x][y].addAdiacente(valori[x-1][y]); // left
+
+          continue;
+        }
+
+        valori[x][y].addAdiacente(valori[x][y+1]); // up
+        valori[x][y].addAdiacente(valori[x][y-1]); // down
+        valori[x][y].addAdiacente(valori[x+1][y]); // right
+        valori[x][y].addAdiacente(valori[x-1][y]); // left
+
+        if (valori[x][y].wormhole) {
+          wormholes.add(valori[x][y]);
+        }
+
       }
     }
+
+    // for (int i = 0; i < wormholes.size(); i++) {
+    //   var worm = wormholes[i]
+    // }
   }
 }

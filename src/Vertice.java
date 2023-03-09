5c2d454e3ba40ceb2ee730548cc622210c507125
Luca Favini <luca.favini.02@gmail.com>
2023-03-09 17:46:09 +0100

    vertice

---
+++ src/Vertice.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertice {
+  final int x;
+  final int y;
+
+  final int valore;
+
+  boolean lock;
+
+  List<Vertice> adiacenti;
+
+  public Vertice(int x, int y, int valore) {
+    this.x = x;
+    this.y = y;
+
+    this.valore = valore;
+
+    this.adiacenti = new ArrayList<>();
+  }
+
+  @Override
+  public String toString() {
+    return x + "-" + y;
+  }
+
+  public void lock() {
+    this.lock = true;
+  }
+
+  public boolean isLocked() {
+    return this.lock;
+  }
+
+  public void addAdiacente(Vertice v) {
+    this.adiacenti.add(v);
+  }
+}

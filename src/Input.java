5d9849f7c969b502e0ba3b58c6c5e8225b1a4963
Matteo Zagheno <matteo2309@hotmail.com>
2023-03-09 17:58:24 +0100

    Added parser

---
+++ src/Input.java
@@ -0,0 +1,16 @@
+public class Input {
+    public int c;
+    public int r;
+    public int s;
+    public int[] serpenti;
+    public int[][] rowMatrix;
+
+    public Input(int c, int r, int s, int[] serpenti, int[][] rowMatrix) {
+        this.c = c;
+        this.r = r;
+        this.s = s;
+        this.serpenti = serpenti;
+        this.rowMatrix = rowMatrix;
+    }
+
+}
---
+++ src/Parser.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Parser {
+
+    public static Input parseInput() {
+        int c;
+        int r;
+        int s;
+        String line;
+        String[] splittedLine;
+        int[] serpenti;
+        int[][] rowMatrix;
+
+        Scanner scanner = new Scanner(System.in);
+        line = scanner.nextLine();
+        splittedLine = line.split(" ");
+        c = Integer.parseInt(splittedLine[0]);
+        r = Integer.parseInt(splittedLine[1]);
+        s = Integer.parseInt(splittedLine[2]);
+
+        serpenti = new int[s];
+        rowMatrix = new int[c][r];
+
+        line = scanner.nextLine();
+        splittedLine = line.split(" ");
+
+        int i = 0;
+        for (String string : splittedLine) {
+            serpenti[i] = Integer.parseInt(string);
+            i++;
+        }
+
+        int y = 0; // numero di riga
+        int x = 0; // numero di colonna
+        while (scanner.hasNext()) {
+            line = scanner.nextLine();
+            splittedLine = line.split(" ");
+            x = 0;
+            for (String token : splittedLine) {
+                if (token == "*")
+                    rowMatrix[x][y] = Integer.MAX_VALUE;
+                else
+                    rowMatrix[x][y] = Integer.parseInt(token);
+                x++;
+            }
+            y++;
+        }
+        return new Input(c, r, s, serpenti, rowMatrix);
+    }
+
+}

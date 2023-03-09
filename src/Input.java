1e89cde904afecf848bcbaede6a870aea00c99a1
Matteo Zagheno <matteo2309@hotmail.com>
2023-03-09 18:05:12 +0100

    added toSting

--- src/Input.java
+++ src/Input.java
@@ -3,14 +3,35 @@ public class Input {
     public int r;
     public int s;
     public int[] serpenti;
-    public int[][] rowMatrix;
+    public int[][] rawMatrix;
 
-    public Input(int c, int r, int s, int[] serpenti, int[][] rowMatrix) {
+    public Input(int c, int r, int s, int[] serpenti, int[][] rawMatrix) {
         this.c = c;
         this.r = r;
         this.s = s;
         this.serpenti = serpenti;
-        this.rowMatrix = rowMatrix;
+        this.rawMatrix = rawMatrix;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("colonne: %d, righe: %d serpenti: %d \n", c, r, s));
+        for (int serp : serpenti) {
+            sb.append(serp);
+            sb.append(' ');
+        }
+        sb.append('\n');
+
+        for (int y = 0; y < r; y++) {
+            for (int x = 0; x < r; r++) {
+                sb.append(rawMatrix[x][y]);
+                sb.append(' ');
+            }
+            sb.append('\n');
+        }
+        sb.append('\n');
+        return sb.toString();
     }
 
 }

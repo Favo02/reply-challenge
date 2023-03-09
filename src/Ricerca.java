5110c02d93e09f3ad9f58918335b8fb32a8d2485
Matteo Zagheno <matteo2309@hotmail.com>
2023-03-09 20:09:08 +0100

    added ricerca

--- src/Ricerca.java
+++ src/Ricerca.java
@@ -1,5 +1,8 @@
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
 public class Ricerca {
     public Grafo g;
@@ -16,12 +19,21 @@ public class Ricerca {
         var maxVer = curVer;
 
         Queue<Vertice> coda = new LinkedList<Vertice>();
+        Set<Vertice> visitati = new HashSet<>();
+        coda.add(curVer);
 
         while (!coda.isEmpty()) {
+            System.out.print(coda);
             curVer = coda.remove();
+            if (visitati.contains(curVer))
+                continue;
+            visitati.add(curVer);
             for (Vertice adiacente : curVer.adiacenti) {
+                if (adiacente.lock == true)
+                    continue;
                 if (adiacente.sommaIntermedia < curVer.sommaIntermedia + adiacente.valore) {
-                    adiacente.step = curVer.step + 1;
+                    if (adiacente.step > curVer.step + 1 || adiacente.step == -1)
+                        adiacente.step = curVer.step + 1;
                     if (adiacente.step > dim) {
                         continue;
                     }
@@ -34,27 +46,57 @@ public class Ricerca {
             }
         }
 
+        System.out.println("\nAAAAAAAAAAAAAA");
+        List<Vertice> tempSerp = new LinkedList<>(); // TODO To remove
         var serpenteFinale = new Serpente(dim);
         serpenteFinale.addVertice(maxVer);
+        tempSerp.add(maxVer);
+        System.out.println("MAX: " + maxVer);
         maxVer.lock = true;
 
         Vertice curMax = maxVer;
         curVer = maxVer;
-
+        System.out.println(curVer);
+        List<Vertice> candidati = new LinkedList<>();
+        int i = 0;
         while (curVer.step != 1) {
-            curMax = curVer.adiacenti.get(0);
+            candidati.clear();
+            System.out.println("-----------------CUR-----------------");
+            System.out.println("cureVer: " + curVer);
+            System.out.println("cureMax: " + curMax);
+            System.out.println("-----------------ADIACENTI-----------------");
             for (Vertice vertice : curVer.adiacenti) {
-                if (vertice.step == curVer.step - 1 && vertice.sommaIntermedia > curMax.sommaIntermedia) {
+                if (vertice.step == curVer.step - 1) {
+                    System.out.println(vertice);
+                    candidati.add(vertice);
+                }
+            }
+            curMax = candidati.get(0);
+            for (Vertice vertice : candidati) {
+                if (vertice.sommaIntermedia > curMax.sommaIntermedia) {
                     curMax = vertice;
                 }
             }
+
             curMax.lock = true;
             serpenteFinale.addVertice(curMax);
+            tempSerp.add(curMax);
             curVer = curMax;
+
+            System.out.println("-----------------PRESI-----------------");
+            System.out.println(tempSerp + "\n");
+            System.out.println("----------------------------------\n\n");
+            // i++;
+            if (i == 3) {
+
+                throw new IllegalArgumentException();
+            }
         }
 
         curVer.lock = true;
-        serpenteFinale.addVertice(curVer);
+        // serpenteFinale.addVertice(curVer);
+        // tempSerp.add(curVer);
+        System.out.println(tempSerp);
         return serpenteFinale;
     }
 }

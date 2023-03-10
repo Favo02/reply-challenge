f88c611b536352806a9d091467f86b2a243cb3a2
Matteo Zagheno <matteo2309@hotmail.com>
2023-03-10 19:03:39 +0100

    added debugToString

--- src/Grafo.java
+++ src/Grafo.java
@@ -9,7 +9,6 @@ public class Grafo {
     valori[fromX][fromY].addAdiacente(valori[toX][toY]);
   }
 
-
   public Grafo(Input input) {
 
     this.valori = new Vertice[input.c][input.r];
@@ -25,97 +24,95 @@ public class Grafo {
       }
     }
 
-    List<Vertice> wormholes = new ArrayList<>(); 
+    List<Vertice> wormholes = new ArrayList<>();
 
     for (int y = 0; y < input.r; y++) {
       for (int x = 0; x < input.c; x++) {
 
         // angolo alto sinistra
         if (y == 0 && x == 0) {
-          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-          valori[x][y].addAdiacente(valori[x][y+1]); // down
-          valori[x][y].addAdiacente(valori[x+1][y]); // right
-          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
-          
+          valori[x][y].addAdiacente(valori[x][input.r - 1]); // up
+          valori[x][y].addAdiacente(valori[x][y + 1]); // down
+          valori[x][y].addAdiacente(valori[x + 1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c - 1][y]); // left
+
           continue;
         }
 
         // angolo alto destra
-        if (y == 0 && x == input.c-1) {
-          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-          valori[x][y].addAdiacente(valori[x][y+1]); // down
+        if (y == 0 && x == input.c - 1) {
+          valori[x][y].addAdiacente(valori[x][input.r - 1]); // up
+          valori[x][y].addAdiacente(valori[x][y + 1]); // down
           valori[x][y].addAdiacente(valori[0][y]); // right
-          valori[x][y].addAdiacente(valori[x-1][y]); // left
-          
+          valori[x][y].addAdiacente(valori[x - 1][y]); // left
+
           continue;
         }
-        
+
         // angolo basso sinistra
-        if (y == input.r-1 && x == 0) {
-          valori[x][y].addAdiacente(valori[x][y-1]); // up
+        if (y == input.r - 1 && x == 0) {
+          valori[x][y].addAdiacente(valori[x][y - 1]); // up
           valori[x][y].addAdiacente(valori[x][0]); // down
-          valori[x][y].addAdiacente(valori[x+1][y]); // right
-          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
-          
+          valori[x][y].addAdiacente(valori[x + 1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c - 1][y]); // left
+
           continue;
         }
-        
+
         // angolo basso destra
-        if (y == input.r-1 && x == input.c-1) {
-          valori[x][y].addAdiacente(valori[x][y-1]); // up
+        if (y == input.r - 1 && x == input.c - 1) {
+          valori[x][y].addAdiacente(valori[x][y - 1]); // up
           valori[x][y].addAdiacente(valori[x][0]); // down
           valori[x][y].addAdiacente(valori[0][y]); // right
-          valori[x][y].addAdiacente(valori[x-1][y]); // left
-          
+          valori[x][y].addAdiacente(valori[x - 1][y]); // left
+
           continue;
         }
-
-
 
         // prima riga
         if (y == 0) {
-          valori[x][y].addAdiacente(valori[x][input.r-1]); // up
-          valori[x][y].addAdiacente(valori[x][y+1]); // down
-          valori[x][y].addAdiacente(valori[x+1][y]); // right
-          valori[x][y].addAdiacente(valori[x-1][y]); // left
-          
+          valori[x][y].addAdiacente(valori[x][input.r - 1]); // up
+          valori[x][y].addAdiacente(valori[x][y + 1]); // down
+          valori[x][y].addAdiacente(valori[x + 1][y]); // right
+          valori[x][y].addAdiacente(valori[x - 1][y]); // left
+
           continue;
         }
 
         // ultima riga
         if (y == input.r - 1) {
-          valori[x][y].addAdiacente(valori[x][y-1]); // up
+          valori[x][y].addAdiacente(valori[x][y - 1]); // up
           valori[x][y].addAdiacente(valori[x][0]); // down
-          valori[x][y].addAdiacente(valori[x+1][y]); // right
-          valori[x][y].addAdiacente(valori[x-1][y]); // left
+          valori[x][y].addAdiacente(valori[x + 1][y]); // right
+          valori[x][y].addAdiacente(valori[x - 1][y]); // left
 
           continue;
         }
 
         // prima colonna
         if (x == 0) {
-          valori[x][y].addAdiacente(valori[x][y-1]); // up
-          valori[x][y].addAdiacente(valori[x][y+1]); // down
-          valori[x][y].addAdiacente(valori[x+1][y]); // right
-          valori[x][y].addAdiacente(valori[input.c-1][y]); // left
+          valori[x][y].addAdiacente(valori[x][y - 1]); // up
+          valori[x][y].addAdiacente(valori[x][y + 1]); // down
+          valori[x][y].addAdiacente(valori[x + 1][y]); // right
+          valori[x][y].addAdiacente(valori[input.c - 1][y]); // left
 
           continue;
         }
 
         // ultima colonna
         if (x == input.c - 1) {
-          valori[x][y].addAdiacente(valori[x][y-1]); // up
-          valori[x][y].addAdiacente(valori[x][y+1]); // down
+          valori[x][y].addAdiacente(valori[x][y - 1]); // up
+          valori[x][y].addAdiacente(valori[x][y + 1]); // down
           valori[x][y].addAdiacente(valori[0][y]); // right
-          valori[x][y].addAdiacente(valori[x-1][y]); // left
+          valori[x][y].addAdiacente(valori[x - 1][y]); // left
 
           continue;
         }
 
-        valori[x][y].addAdiacente(valori[x][y-1]); // up
-        valori[x][y].addAdiacente(valori[x][y+1]); // down
-        valori[x][y].addAdiacente(valori[x+1][y]); // right
-        valori[x][y].addAdiacente(valori[x-1][y]); // left
+        valori[x][y].addAdiacente(valori[x][y - 1]); // up
+        valori[x][y].addAdiacente(valori[x][y + 1]); // down
+        valori[x][y].addAdiacente(valori[x + 1][y]); // right
+        valori[x][y].addAdiacente(valori[x - 1][y]); // left
 
         if (valori[x][y].wormhole) {
           wormholes.add(valori[x][y]);
@@ -137,9 +134,21 @@ public class Grafo {
 
           vertice.addAdiacente(wormholes.get(j));
         }
-        
+
       }
     }
+  }
+
+  public String debugToString() {
+    StringBuilder sb = new StringBuilder();
+    for (int y = 0; y < valori[0].length; y++) {
+      for (int x = 0; x < valori.length; x++) {
+        sb.append(valori[x][y]);
+        sb.append(' ');
+      }
+      sb.append('\n');
+    }
+    return sb.toString();
   }
 
   public String toString() {
